f0588419c8538de30ae21127271175366458940b
Kevin Brunel <brunel.k.34@gmail.com>
2024-01-26 14:37:49 +0100

    Ajout des error_log dans la table scrapy_errors

--- craiglist/craiglist/pipelines.py
+++ craiglist/craiglist/pipelines.py
@@ -1,152 +1,10 @@
-# # # Define your item pipelines here
-# # #
-# # # Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# # # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
-
-# # # useful for handling different item types with a single interface
-# # import pymongo
-# # from itemadapter import ItemAdapter
-
-
-# # class MongoPipeline:
-# #     collection_name = "scrapy_citations"
-
-# #     def __init__(self, mongo_uri, mongo_db):
-# #         self.mongo_uri = mongo_uri
-# #         self.mongo_db = mongo_db
-
-# #     @classmethod
-# #     def from_crawler(cls, crawler):
-# #         return cls(
-# #             mongo_uri=crawler.settings.get("MONGO_URI"),
-# #             mongo_db=crawler.settings.get("MONGO_DATABASE", "DB_SRAPY"),
-# #         )
-
-# #     def open_spider(self, spider):
-# #         self.client = pymongo.MongoClient(self.mongo_uri)
-# #         self.db = self.client[self.mongo_db]
-
-# #     def close_spider(self, spider):
-# #         self.client.close()
-
-# #     def process_item(self, item, spider):
-# #         self.db[self.collection_name].insert_one(ItemAdapter(item).asdict())
-# #         return item
-
-
-# # # Define your item pipelines here
-# # #
-# # # Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# # # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
-
-# # # useful for handling different item types with a single interface
-# import pymongo
-# from itemadapter import ItemAdapter
-# from pymongo.errors import DuplicateKeyError
-
-# class MongoPipeline:
-#     collection_name = "scrapy_citations"
-
-#     def __init__(self, mongo_uri, mongo_db):
-#         self.mongo_uri = mongo_uri
-#         self.mongo_db = mongo_db
-
-#     @classmethod
-#     def from_crawler(cls, crawler):
-#         return cls(
-#             mongo_uri=crawler.settings.get("MONGO_URI"),
-#             mongo_db=crawler.settings.get("MONGO_DATABASE", "DB_SRAPY"),
-#         )
-
-#     def open_spider(self, spider):
-#         self.client = pymongo.MongoClient(self.mongo_uri)
-#         self.db = self.client[self.mongo_db]
-#         self.collection = self.db[self.collection_name]
-#         self.collection.create_index("citation", unique=True)  # Crée un index unique sur le champ de la citation
-
-#     def close_spider(self, spider):
-#         self.client.close()
-
-#     def process_item(self, item, spider):
-#         try:
-#             self.collection.insert_one(ItemAdapter(item).asdict())
-#         except DuplicateKeyError:
-#             spider.logger.warning(f"Duplicate item found: {item!r}")
-
-#         return item
-
-#a garder
-# import pymongo
-# from itemadapter import ItemAdapter
-# from pymongo.errors import DuplicateKeyError
-# from fuzzywuzzy import fuzz
-
-# class MongoPipeline:
-#     collection_name = "scrapy_citations"
-
-#     def __init__(self, mongo_uri, mongo_db):
-#         self.mongo_uri = mongo_uri
-#         self.mongo_db = mongo_db
-
-#     @classmethod
-#     def from_crawler(cls, crawler):
-#         return cls(
-#             mongo_uri=crawler.settings.get("MONGO_URI"),
-#             mongo_db=crawler.settings.get("MONGO_DATABASE", "DB_SRAPY"),
-#         )
-
-#     def open_spider(self, spider):
-#         self.client = pymongo.MongoClient(self.mongo_uri)
-#         self.db = self.client[self.mongo_db]
-#         self.collection = self.db[self.collection_name]
-#         self.collection.create_index("citation", unique=True)
-#         self.collection.create_index("auteur")  # Crée un index sur le champ "auteur"
-
-#     def close_spider(self, spider):
-#         self.client.close()
-
-#     def process_item(self, item, spider):
-#         citation = item['citation']
-#         auteur = item['auteur']
-
-#         # La citation n'est pas trouvée dans la base parmi les citations de cet auteur
-#         if not self.collection.find_one({'auteur': auteur, 'citation': citation}):
-#             # Recherche de citations similaires de cet auteur dans la base de données
-#             similar_citations = self.collection.find({
-#                 'auteur': auteur,
-#                 'citation': {'$regex': citation, '$options': 'i'}
-#             })
-
-#             for similar_citation in similar_citations:
-#                 similarity_score = fuzz.ratio(citation, similar_citation['citation'])
-#                 # Si des citations similaires sont trouvées mise à jour (seuil de similarité de 80%)
-#                 if similarity_score > 80:  
-#                     spider.logger.info(f"Updating similar item: {similar_citation['citation']} with {citation}")
-#                     try:
-#                         self.collection.update_one({'_id': similar_citation['_id']}, {'$set': ItemAdapter(item).asdict()})
-#                         return item  # Retourne après la mise à jour, évitant l'insertion
-#                     except DuplicateKeyError:
-#                         spider.logger.warning(f"Duplicate item found during update: {item!r}")
-
-#             # Cas 3: La citation n'existe pas dans la base, l'insérer
-#             try:
-#                 self.collection.insert_one(ItemAdapter(item).asdict())
-#             except DuplicateKeyError:
-#                 spider.logger.warning(f"Duplicate item found during insert: {item!r}")
-
-#         return item
-
-
-
 import pymongo
 from itemadapter import ItemAdapter
-from pymongo.errors import DuplicateKeyError
-from difflib import SequenceMatcher
+from scrapy import signals
 
 class MongoPipeline:
     collection_name = "scrapy_citations"
+    errors_collection_name = "scrapy_errors"
 
     def __init__(self, mongo_uri, mongo_db):
         self.mongo_uri = mongo_uri
@@ -154,47 +12,38 @@ class MongoPipeline:
 
     @classmethod
     def from_crawler(cls, crawler):
-        return cls(
+        pipeline = cls(
             mongo_uri=crawler.settings.get("MONGO_URI"),
-            mongo_db=crawler.settings.get("MONGO_DATABASE", "DB_SRAPY"),
+            mongo_db=crawler.settings.get("MONGO_DATABASE", "DB_SCRAPY"),
         )
+        crawler.signals.connect(pipeline.spider_error, signal=signals.spider_error)
+        return pipeline
 
     def open_spider(self, spider):
         self.client = pymongo.MongoClient(self.mongo_uri)
         self.db = self.client[self.mongo_db]
         self.collection = self.db[self.collection_name]
-        self.collection.create_index("citation", unique=True)
-        self.collection.create_index("auteur")  # Crée un index sur le champ "auteur"
+        self.errors_collection = self.db[self.errors_collection_name]
+        self.collection.create_index([("citation", pymongo.TEXT)], name='citation_index', unique=True)
+        self.collection.create_index([("auteur", pymongo.ASCENDING)], name='auteur_index')
 
     def close_spider(self, spider):
         self.client.close()
 
     def process_item(self, item, spider):
-        citation = item['citation']
-        auteur = item['auteur']
-
-        # La citation n'est pas trouvée dans la base parmi les citations de cet auteur
-        if not self.collection.find_one({'auteur': auteur, 'citation': citation}):
-            # Recherche de citations similaires de cet auteur dans la base de données
-            similar_citations = self.collection.find({
-                'auteur': auteur,
-            })
-
-            for similar_citation in similar_citations:
-                similarity_score = SequenceMatcher(None, citation, similar_citation['citation']).ratio()
-                # Si des citations similaires sont trouvées mise à jour (seuil de similarité de 80%)
-                if similarity_score > 0.8:  
-                    spider.logger.info(f"Updating similar item: {similar_citation['citation']} with {citation}")
-                    try:
-                        self.collection.update_one({'_id': similar_citation['_id']}, {'$set': ItemAdapter(item).asdict()})
-                        return item  # Retourne après la mise à jour, évitant l'insertion
-                    except DuplicateKeyError:
-                        spider.logger.warning(f"Duplicate item found during update: {item!r}")
-
-            # Cas 3: La citation n'existe pas dans la base, l'insérer
-            try:
-                self.collection.insert_one(ItemAdapter(item).asdict())
-            except DuplicateKeyError:
-                spider.logger.warning(f"Duplicate item found during insert: {item!r}")
-
+        try:
+            self.collection.insert_one(ItemAdapter(item).asdict())
+        except Exception as e:
+            self.errors_collection.insert_one({'error': str(e), 'item': dict(item)})
+            spider.logger.error(f"Error processing item: {e}")
         return item
+
+
+    def spider_error(self, failure, response, spider):
+        error_info = {
+            'url': response.url,
+            'exception': failure.getTraceback(),
+        }
+        self.errors_collection.insert_one(error_info)
+        spider.logger.error(f"Error on {response.url}: {failure.getTraceback()}")
+
--- craiglist/craiglist/spiders/jobs.py
+++ craiglist/craiglist/spiders/jobs.py
@@ -1,61 +1,32 @@
 import scrapy
 
+class JobsSpider(scrapy.Spider):
+    name = "jobs"
+import scrapy
 
 class JobsSpider(scrapy.Spider):
     name = "jobs"
 
-    def start_requests(self): #Crawler de page en page
+    def start_requests(self):
         urls = [
             "https://quotes.toscrape.com/"
         ]
         for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+            yield scrapy.Request(url=url, callback=self.parse, errback=self.handle_error)
 
     def parse(self, response):
         citations = response.xpath('//*[@class="quote"]')
         for citation in citations:
-            myCitations = {
+            yield {
                 "auteur": citation.xpath('.//*[@class="author"]/text()').get(),
-                "citation": citation.xpath('.//*[@class="text"]/text()').get()
+                "citation": citation.xpath('.//*[@class="text"]/text()').get(),
             }
-            yield myCitations
-        # go to the next page
-        url = response.css(".next > a::attr(href)").get()
-        if url is not None:
-            yield response.follow(url=url, callback=self.parse)
 
+        # Pagination
+        # Pagination
+        next_page = response.css('li.next > a::attr(href)').get()
+        if next_page is not None:
+            yield response.follow(next_page, self.parse)
 
-# VERSION qui Modifie une citation pour vérifier le bon fonctionnement du pipeline (update le champs si similarité > 80%)
-# import scrapy
-
-# class JobsSpider(scrapy.Spider):
-#     name = "jobs"
-
-#     def start_requests(self):  # Crawler de page en page
-#         urls = [
-#             "https://quotes.toscrape.com/"
-#         ]
-#         for url in urls:
-#             yield scrapy.Request(url=url, callback=self.parse)
-
-#     def parse(self, response):
-#         citations = response.xpath('//*[@class="quote"]')
-#         for citation in citations:
-#             myTest = citation.xpath('.//*[@class="text"]/text()').get()
-
-#             # Ajoutez une condition pour vérifier si la citation correspond à celle spécifiée
-#             if "The world as we have created it is a process of our thinking. It cannot be changed without changing our thinking." in myTest:
-#                 self.logger.info(f"Original citation: {myTest}")
-#                 myTest = "The world as we have created it is a process of our thinking. It cannot be changed without changing our thinking test !!!"
-#                 self.logger.info(f"Modified citation: {myTest}")
-
-#             myCitations = {
-#                 "auteur": citation.xpath('.//*[@class="author"]/text()').get(),
-#                 "citation": myTest
-#             }
-#             yield myCitations
-
-#         # Passer à la page suivante
-#         url = response.css(".next > a::attr(href)").get()
-#         if url is not None:
-#             yield response.follow(url=url, callback=self.parse)
+    def handle_error(self, failure):
+        self.logger.error(f'Request failed: {failure.request.url}')
